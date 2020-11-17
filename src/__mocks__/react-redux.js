9df12791ce2d8e892677b899d145d92c2e592a14
Daniel Ruiz Cayuela <druiz@financialforce.com>
2020-11-17 17:05:03 +0100

    Add Presentation test

--- src/__mocks__/react-redux.js
+++ src/__mocks__/react-redux.js
@@ -1,6 +1,8 @@
 const mockDispatch = jest.fn()
 const mockSelector = jest.fn()
  
+// it has to be here because: https://github.com/facebook/create-react-app/issues/7539
+
 const mockState = (state = {}) => {
     return mockSelector.mockImplementationOnce(callback => {
         return callback(state);
--- src/components/presentation/__tests__/Presentation.test.js
+++ src/components/presentation/__tests__/Presentation.test.js
@@ -1,16 +1,30 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import Presentation from '../Presentation';
-import App from '../../App';
+import Counter from '../../counter/Counter';
 
-let wrapped = mount(
-	<App>
-		<Presentation />
-	</App>
-);
+jest.doMock('../../counter/Counter', () => {
+	const Counter = () => <div />;
+	return Counter;
+});
+
 describe('Presentation', () => {
 	
-  it('should render the header component', () => {
+  it('should render only the header component', () => {
+	let wrapped = shallow(
+		<Presentation />
+	);
     expect(wrapped.find('Header').length).toEqual(1);
+    expect(wrapped.find('Counter').length).toEqual(0);
+  });
+
+  it('should render the header plus children components', () => {
+	let wrapped = shallow(
+		<Presentation >
+			<Counter></Counter>
+		</Presentation>
+	);
+    expect(wrapped.find('Header').length).toEqual(1);
+    expect(wrapped.find('Counter').length).toEqual(1);
   });
 });
